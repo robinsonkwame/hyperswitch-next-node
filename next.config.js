11da4ed4c6e4f5aa369049b2855c31311c7876b9
Arush <arush.kapoor@juspay.in>
2023-07-25 13:33:51 +0530

    Added server.js file to mock requests between Client and Server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@juspay-tech/hyper-js', '@juspay-tech/react-hyper-js'],
+  async rewrites() {
+    return [
+      {
+        source: '/create-payment',
+        destination: 'http://localhost:4242/create-payment' // Proxy to Backend
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
